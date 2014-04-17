f7344ac297fa6045d41c2dea65406aef066b86ae
majj <aidear@163.com>
2014-04-18 00:08:56 +0800

    refactor main.py

--- mabozen/main.py
+++ mabozen/main.py
@@ -9,9 +9,7 @@ command line
 
 import argparse
 
-parser = argparse.ArgumentParser(description='mabozen command line.')
-
-parser.add_argument('--noarg', action="store_false", default=True)
+parser = argparse.ArgumentParser(prog='mabozen.py', description='mabozen command line.')
 
 parser.add_argument('-j', '--tojson', dest='tojson', action='store_true',
                    help='from pg db schema to json', default=False)
@@ -22,24 +20,14 @@ parser.add_argument('-b', '--backup', dest='backup', action='store_true',
 parser.add_argument('-l', '--load', dest='load', action='store_true',
                    help='load function to db', default=False)
                    
-parser.add_argument('-v', '--version', action='version', version='mabozen 0.0.1')                   
+parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.0.1')                   
                    
 args = parser.parse_args()
 
-if args.noarg:
-    logo = """
-        \      /
-        --**--
-        | O O |  \\
-        \     /   |\\
-         [   ]
-         - - -    
-    """
-    print logo
-    print parser.parse_args(['-h'])
+NOARG = True
 
 if args.tojson:
-    
+    NOARG = False
     from mabozen.schema2json import JsonModels
     
     print("to json")
@@ -47,7 +35,7 @@ if args.tojson:
     jsonm.run()
 
 if args.backup:
-    
+    NOARG = False    
     from mabozen.pgfunc_backup import PgFunction
     
     print("backup")
@@ -56,8 +44,16 @@ if args.backup:
     
 if args.load:
     
+    NOARG = False
+    
     from mabozen.function_loader import Loader
     
     print("load")
     loader = Loader()
     loader.run()
+    
+if NOARG:
+    parser.print_help()
+
+
+
